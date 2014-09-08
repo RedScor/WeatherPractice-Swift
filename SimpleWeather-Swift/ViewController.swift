aa1aa8770f10258653531d995cd0fa88e65756b8
Redscor <yuanruo@gmail.com>
2014-09-08 15:04:26 +0800

    update to Xcode 6 beta7 and add blurView

--- SimpleWeather-Swift/ViewController.swift
+++ SimpleWeather-Swift/ViewController.swift
@@ -8,10 +8,10 @@
 
 import UIKit
 
-class ViewController: UIViewController,UITableViewDataSource,UITableViewDelegate,UIScrollViewDelegate {
+ class ViewController: UIViewController,UITableViewDataSource,UITableViewDelegate,UIScrollViewDelegate {
     
     @IBOutlet weak var backgroundImageView: UIImageView! = UIImageView()
-    var blurredImageView: UIImageView?
+//    var blurredImageView: UIImageView?
     @IBOutlet var tableView: UITableView! = UITableView()
     
     @IBOutlet weak var cityLabel: UILabel! = UILabel()
@@ -19,7 +19,7 @@ class ViewController: UIViewController,UITableViewDataSource,UITableViewDelegate
     @IBOutlet weak var conditionsLabel: UILabel! = UILabel()
     @IBOutlet weak var temperatureLabel: UILabel! = UILabel()
     @IBOutlet weak var hiloLabel: UILabel! = UILabel()
-    
+    let blurView:UIVisualEffectView = UIVisualEffectView(effect: UIBlurEffect(style: UIBlurEffectStyle.Light))
     
     var screenHeight: CGFloat
     let client = APIClient()
@@ -37,6 +37,12 @@ class ViewController: UIViewController,UITableViewDataSource,UITableViewDelegate
     override func viewDidLoad() {
         super.viewDidLoad()
         
+//        blurView:UIVisualEffectView = UIVisualEffectView(effect: UIBlurEffect(style: UIBlurEffectStyle.Light))
+        blurView.frame = self.view.bounds
+        blurView.alpha = 0.0;
+        view.addSubview(blurView)
+        self.view .bringSubviewToFront(tableView)
+        
         tableView.delegate = self
         tableView.dataSource = self
         view.backgroundColor = UIColor.redColor()
@@ -51,7 +57,7 @@ class ViewController: UIViewController,UITableViewDataSource,UITableViewDelegate
         println("getCurrentCondition")
         let manager = WeatherManager.instance.currentConditionData
         cityLabel.text = manager?.city
-        iconView.image = UIImage(named: manager?.imageName())
+        iconView.image = UIImage(named: manager!.imageName()!)
         conditionsLabel.text = manager?.weatherMain
         temperatureLabel.text = "\(Int(manager!.celsius))°"
         hiloLabel.text = "\(manager!.celsiusMax)°/\(manager!.celsiusMin)°"
@@ -72,8 +78,7 @@ class ViewController: UIViewController,UITableViewDataSource,UITableViewDelegate
         return 2
     }
     
-    func tableView(tableView: UITableView!, numberOfRowsInSection section: Int) -> Int {
-
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         if (section == 0) {
             if let hourlyCount =  WeatherManager.instance.hourlyForecastData?.count {
                 return min(hourlyCount, 6) + 1
@@ -85,19 +90,21 @@ class ViewController: UIViewController,UITableViewDataSource,UITableViewDelegate
         return 0
     }
     
-    func tableView(tableView: UITableView!, cellForRowAtIndexPath indexPath: NSIndexPath!) -> UITableViewCell! {
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let identifier = "cellIdentifier"
-        var cell = tableView.dequeueReusableCellWithIdentifier(identifier) as? UITableViewCell
-        if cell == nil {
-            cell = UITableViewCell(style: .Value1, reuseIdentifier: identifier)
-        }
-        cell!.selectionStyle = .None
-        cell!.backgroundColor = UIColor(red: 0.0, green: 0.0, blue: 0.0, alpha: 0.2)
-        cell!.textLabel.textColor = UIColor.whiteColor()
-        cell!.detailTextLabel.textColor = UIColor.whiteColor()
+//        let cell = tableView.dequeueReusableCellWithIdentifier(identifier) as UITableViewCell
+        let cell = tableView.dequeueReusableCellWithIdentifier(identifier, forIndexPath: indexPath) as UITableViewCell
+//        if cell == nil {
+//            cell = UITableViewCell(style: .Value1, reuseIdentifier: identifier)
+//        }
+        cell.selectionStyle = .None
+    
+        cell.backgroundColor = UIColor(red: 0.0, green: 0.0, blue: 0.0, alpha: 0.2)
+        cell.textLabel?.textColor = UIColor.whiteColor()
+        cell.detailTextLabel?.textColor = UIColor.whiteColor()
         
         if indexPath.row == 0 {
-            configureHeaderCell(cell!, titleIndex: indexPath.section)
+            configureHeaderCell(cell, titleIndex: indexPath.section)
         } else {
             var model: WeatherModel?
             
@@ -109,7 +116,7 @@ class ViewController: UIViewController,UITableViewDataSource,UITableViewDelegate
             
             let selectCell = selectHourlyAndDailyCell(indexPath.section)
             if let dataModel = model {
-                selectCell(cell!, dataModel)
+                selectCell(cell, dataModel)
             }
         }
 
@@ -125,30 +132,38 @@ class ViewController: UIViewController,UITableViewDataSource,UITableViewDelegate
             titleStr = "Daily Forecast"
 
         }
-        cell.textLabel.font = UIFont.systemFontOfSize(18.0)
-        cell.textLabel.text = titleStr
-        cell.detailTextLabel.text = ""
-        cell.imageView.image = nil
+        cell.textLabel!.font = UIFont.systemFontOfSize(18.0)
+        cell.textLabel!.text = titleStr
+        cell.detailTextLabel!.text = ""
+        cell.imageView!.image = nil
     }
     
     
     func selectHourlyAndDailyCell(cellSection:Int) -> (UITableViewCell,WeatherModel) -> Void {
         func configureHourlyCell(cell:UITableViewCell,weather:WeatherModel) {
-            cell.textLabel.font = UIFont.systemFontOfSize(18)
-            cell.detailTextLabel.font = UIFont.systemFontOfSize(18)
-            cell.textLabel.text = WeatherManager.instance.hourlyFormatter.stringFromDate(weather.timeDate)
-            cell.detailTextLabel.text = NSString(format: "%.1f°", weather.celsius)
-            cell.imageView.image = UIImage(named: weather.imageName())
-            cell.imageView.contentMode = .ScaleAspectFit
+            cell.textLabel!.font = UIFont.systemFontOfSize(18)
+            cell.detailTextLabel!.font = UIFont.systemFontOfSize(18)
+            cell.textLabel!.text = WeatherManager.instance.hourlyFormatter.stringFromDate(weather.timeDate)
+            cell.detailTextLabel!.text = NSString(format: "%.1f°", weather.celsius)
+            if let image = weather.imageName() {
+                cell.imageView!.image = UIImage(named: image)
+            }else {
+                cell.imageView?.image = nil
+            }
+            cell.imageView!.contentMode = .ScaleAspectFit
         }
         
         func configureDailyCell(cell: UITableViewCell,weather: WeatherModel) {
-            cell.textLabel.font = UIFont.systemFontOfSize(18)
-            cell.detailTextLabel.font = UIFont.systemFontOfSize(18)
-            cell.textLabel.text = WeatherManager.instance.dailyFormatter.stringFromDate(weather.timeDate)
-            cell.detailTextLabel.text = NSString(format: "%.0f° / %.0f°", weather.celsiusMax,weather.celsiusMin)
-            cell.imageView.image = UIImage(named: weather.imageName())
-            cell.imageView.contentMode = .ScaleAspectFit
+            cell.textLabel!.font = UIFont.systemFontOfSize(18)
+            cell.detailTextLabel!.font = UIFont.systemFontOfSize(18)
+            cell.textLabel!.text = WeatherManager.instance.dailyFormatter.stringFromDate(weather.timeDate)
+            cell.detailTextLabel!.text = NSString(format: "%.0f° / %.0f°", weather.celsiusMax,weather.celsiusMin)
+            if let image = weather.imageName() {
+                cell.imageView!.image = UIImage(named: image)
+            }else {
+                cell.imageView?.image = nil
+            }
+            cell.imageView!.contentMode = .ScaleAspectFit
         }
         
         return cellSection == 0 ? configureHourlyCell : configureDailyCell
@@ -170,6 +185,17 @@ class ViewController: UIViewController,UITableViewDataSource,UITableViewDelegate
         
         backgroundImageView!.frame = bounds
         tableView.frame = bounds
+    }
+    
+    func scrollViewDidScroll(scrollView: UIScrollView) {
+        let height:CGFloat = scrollView.bounds.size.height;
+        let position:CGFloat = max(scrollView.contentOffset.y, 0.0);
+        
+        // 2.偏移量除以高度，並且最大值為1，所以alpha上限為1。
+        let percent:CGFloat = min(position / height, 1.0);
+        
+        // 3.當你滾動的時候，把結果值賦給模糊圖像的alpha屬性，來更改模糊圖像。
+        self.blurView.alpha = percent;
     }
 
     override func didReceiveMemoryWarning() {
